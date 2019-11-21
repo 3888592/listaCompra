1fe04d299debdcf3f6ee119c2435142a3fd4ce8c
Mathias <3888592@alu.murciaeduca.es>
2019-11-21 08:41:11 +0100

    Ejercicio ListaCompra II

--- resources/views/productos/show.blade.php
+++ resources/views/productos/show.blade.php
@@ -2,6 +2,23 @@
 
 @section('content')
 
-    Productos show {{$id}}
+<div class="row">
+
+    <div class="col-sm-4">
+
+        <img src="https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwimneP27fjlAhULVhoKHWrgAGYQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.xataka.com%2Fespacio%2Fhan-sido-necesarias-50-000-fotos-para-capturar-esta-alucinante-imagen-81-mpixeles-luna&psig=AOvVaw2R7KyA1mLL9EqhbzCy-RLX&ust=1574341679054399">
+
+    </div>
+    <div class="col-sm-8">
+
+        <h1>{{$arrayProductos[0]}}</h1>
+        <h3>Descripción: {{$arrayProductos[1]}}</h3>
+        <h3>Estado: Actualmente agotado</h3>
+        <a href="" class="btn btn-danger">Pendiente de compra</a>
+        <a href="/productos/edit/{{$id}}" class="btn btn-warning">Editar Película</a>
+        <a href="/productos" class="btn btn-dark">Volver al listado</a>
+
+    </div>
+</div>
 
 @stop
